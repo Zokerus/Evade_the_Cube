be3405531b33c3a4298ce9b71e2ec777fded2e70
Zokerus <Zokerus@users.noreply.github.com>
2023-11-11 13:38:27 +0100

    delete z-axis on player movement

--- Player.gd
+++ Player.gd
@@ -9,14 +9,12 @@ signal health_changed(hp: int)
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("Left", "Right","ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, 0)).normalized()
+	var input_dir : float = Input.get_axis("Left", "Right")
+	var direction = (transform.basis * Vector3(input_dir, 0, 0)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
 
